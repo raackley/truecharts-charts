7eae82f05d36fe2638f4612b018993231930e00e
Ryan Ackley <raackley@protonmail.com>
2022-12-13 00:52:15 -0500

    stable

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -5,7 +5,7 @@ set -e
 for CHART in $(ls charts/stable); do
     echo "#####"
     echo "Chart: ${CHART}"
-    VERSION=$(cat charts/${CHART}/Chart.yaml | grep version: | cut -d' ' -f2)
+    VERSION=$(cat charts/stable/${CHART}/Chart.yaml | grep version: | cut -d' ' -f2)
     echo "Version: ${VERSION}"
 
     set +e
@@ -15,8 +15,8 @@ for CHART in $(ls charts/stable); do
 
     if [ $CHECK_VERSION -ne 0 ]; then
         echo "Building and uploading chart ${CHART} version ${VERSION} to ${REPO_URL}."
-        helm dependency update charts/${CHART}/
-        helm package charts/${CHART}/
+        helm dependency update charts/stable/${CHART}/
+        helm package charts/stable/${CHART}/
         echo curl --data-binary "@${CHART}-${VERSION}.tgz" ${REPO_URL}/api/charts -u ${USER}:${PASS}
     else
         echo "Chart ${CHART} version ${VERSION} already exists in ${REPO_URL}, skipping."
